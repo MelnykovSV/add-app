42f9d2c8bb379195b55b04cf730ad272ffc599ce
MelnykovSV <svmelnykov@gmail.com>
2024-04-22 18:41:08 +0300

    configured project

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["airbnb", "airbnb/hooks", "airbnb-typescript"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "import/no-absolute-path": "off",
+    "linebreak-style": "off",
+    "object-curly-newline": "off",
+    "react/jsx-props-no-spreading": "off",
+  },
+};
